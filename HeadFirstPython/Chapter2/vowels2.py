09a18f935a46ede230b7534804ed84308df3e68e
Tiago Saraiva <tiagofsaraiva@hotmail.com>
2024-01-16 18:25:55 -0600

    Last vowel exercise

---
+++ HeadFirstPython/Chapter2/vowels2.py
@@ -0,0 +1,14 @@
+"""
+Book Head First Python, chapter 2, example, page 60
+
+@author: tiago
+@date: Jan 24
+"""
+vowels = ['a', 'e', 'i', 'o', 'u']
+word = "Milliways"
+
+for letter in word:
+   if letter in vowels:
+       print(letter)
+
+
